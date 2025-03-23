ba86da14fc9c63702e0e32fea41550dee851adbc
anant <anantkumar312@gmail.com>
2025-03-23 16:14:37 +0530

    chore: remove unnecessary expiry code

--- src/components/list/ListHeader.jsx
+++ src/components/list/ListHeader.jsx
@@ -1,19 +1,15 @@
 import React from "react";
+import { AlertIcon } from "../ui/icons";
 
-export default function ListHeader({ id, expired, expiryDate }) {
+export default function ListHeader({ id, expiryDate }) {
   return (
     <>
-      <h1
-        className={`font-bold ${
-          expired ? "text-red-500 text-2xl" : "text-2xl md:text-5xl text-white"
-        }`}
-      >
-        {expired ? "This List Has Expired" : "Join List"}
-      </h1>
+      <h1 className={"font-bold text-2xl md:text-5xl"}>Join List</h1>
       <span className="text-sm mt-1 text-neutral-500">{id}</span>
-      <span className="text-sm mt-1 text-red-100">
-        List expires on {expiryDate}
-      </span>
+      <div className="flex gap-1 text-red-100 text-sm mt-1 items-center">
+        <AlertIcon />
+        <span>List expires on {expiryDate}</span>
+      </div>
     </>
   );
 }
